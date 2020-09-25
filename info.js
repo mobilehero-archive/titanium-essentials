c34df93747849b7a5458cfc668447338d411c25f
Brenton House <brenton.house@gmail.com>
2020-09-24 23:06:55 -0500

    :thumbsup: v1.0.2 :heavy_minus_sign: Bumping version for project

--- info.js
+++ info.js
@@ -28,7 +28,7 @@ const info = {
 			return 'Unknown';
 		 }
 
-		return `${parseFloat(battery_level).toFixed(2)}%`;
+		return `${parseFloat(battery_level * 100).toFixed(2)}%`;
 	},
 	get device_available_memory() {
 		return Ti.Platform.availableMemory;
